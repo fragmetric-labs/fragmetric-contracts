5abbb28e90c1557211d1f41df094450f1ffd9bd7
천정현 <ouoabcde@naver.com>
2024-05-24 14:39:30 +0900

    test: add user2 incrementing, and check user1's data is safe

--- tests/deposit-program.ts
+++ tests/deposit-program.ts
@@ -11,6 +11,7 @@ describe("deposit-program", () => {
   const program = anchor.workspace.DepositProgram as Program<DepositProgram>;
 
   const user1 = anchor.web3.Keypair.generate();
+  const user2 = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // airdrop some SOL to new user
@@ -88,6 +89,53 @@ describe("deposit-program", () => {
 
     const account1 = await program.account.userTokenAmount.fetch(user1.publicKey);
     console.log("Updated amount:", account1.amount.toNumber());
+    expect(account1.amount.toNumber()).to.equal(120);
+  });
+
+  it("Initialize user2", async () => {
+    // airdrop some SOL to new user
+    const airdropSignature = await provider.connection.requestAirdrop(
+      user2.publicKey,
+      4 * web3.LAMPORTS_PER_SOL // 4 SOL
+    );
+
+    // confirm the transaction
+    await provider.connection.confirmTransaction(airdropSignature);
+
+    // check the balance
+    const user2Bal = await provider.connection.getBalance(user2.publicKey);
+    console.log(`User2 SOL balance: ${user2Bal}`);
+
+    const tx = await program.methods
+      .initialize()
+      .accounts({ userTokenAmount: user2.publicKey })
+      .signers([user2])
+      .rpc();
+    console.log("Initialize transaction signature", tx);
+
+    const account2 = await program.account.userTokenAmount.fetch(user2.publicKey);
+    expect(account2.amount.toNumber()).to.equal(0);
+  });
+
+  it("Incremented user2's token amount", async () => {
+    const data = {
+      token: "LST1",
+      amount: new anchor.BN(50)
+    };
+
+    const tx = await program.methods
+      .increment(data)
+      .accounts({ userTokenAmount: user2.publicKey, user: user2.publicKey })
+      .signers([user2])
+      .rpc();
+    console.log("Increment transaction:", tx);
+
+    const account2 = await program.account.userTokenAmount.fetch(user2.publicKey);
+    console.log("Updated amount:", account2.amount.toNumber());
+    expect(account2.amount.toNumber()).to.equal(50);
+
+    const account1 = await program.account.userTokenAmount.fetch(user1.publicKey);
+    console.log("Check user1's amount is not changed:", account1.amount.toNumber());
     expect(account1.amount.toNumber()).to.equal(120);
   });
 });
