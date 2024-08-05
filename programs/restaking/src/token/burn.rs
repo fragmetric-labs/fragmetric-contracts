fad16f23814c28b9fb11d9438a0d4235965b660a
Kangji <drcotr0623@naver.com>
2024-08-05 14:22:33 +0900

    fix: split mint and burn

--- programs/restaking/src/token/burn.rs
+++ programs/restaking/src/token/burn.rs
@@ -1,45 +1,45 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     token_2022::Token2022,
-    token_interface::{mint_to, Mint, MintTo, TokenAccount},
+    token_interface::{burn, Burn, Mint, TokenAccount},
 };
 
-pub(crate) trait MintExt<'info>
+pub(crate) trait BurnExt<'info>
 where
     Self: 'info,
 {
-    fn mint_token_cpi(
+    fn burn_token_cpi(
         &self,
         mint: &InterfaceAccount<'info, Mint>,
-        to: &InterfaceAccount<'info, TokenAccount>,
+        from: &InterfaceAccount<'info, TokenAccount>,
         authority: AccountInfo<'info>,
         signer_seeds: Option<&[&[&[u8]]]>,
         amount: u64,
     ) -> Result<()>;
 }
 
-impl<'info> MintExt<'info> for Program<'info, Token2022> {
-    fn mint_token_cpi(
+impl<'info> BurnExt<'info> for Program<'info, Token2022> {
+    fn burn_token_cpi(
         &self,
         mint: &InterfaceAccount<'info, Mint>,
-        to: &InterfaceAccount<'info, TokenAccount>,
+        from: &InterfaceAccount<'info, TokenAccount>,
         authority: AccountInfo<'info>,
         signer_seeds: Option<&[&[&[u8]]]>,
         amount: u64,
     ) -> Result<()> {
-        let mut mint_receipt_token_cpi_ctx = CpiContext::new(
+        let mut burn_receipt_token_cpi_ctx = CpiContext::new(
             self.to_account_info(),
-            MintTo {
+            Burn {
                 mint: mint.to_account_info(),
-                to: to.to_account_info(),
+                from: from.to_account_info(),
                 authority,
             },
         );
 
         if let Some(signer_seeds) = signer_seeds {
-            mint_receipt_token_cpi_ctx = mint_receipt_token_cpi_ctx.with_signer(signer_seeds);
+            burn_receipt_token_cpi_ctx = burn_receipt_token_cpi_ctx.with_signer(signer_seeds);
         }
 
-        mint_to(mint_receipt_token_cpi_ctx, amount)
+        burn(burn_receipt_token_cpi_ctx, amount)
     }
 }
--- programs/restaking/src/token/mod.rs
+++ programs/restaking/src/token/mod.rs
@@ -2,8 +2,10 @@ mod instructions;
 
 pub use instructions::*;
 
+mod burn;
 mod mint;
 mod transfer;
 
+pub(crate) use burn::*;
 pub(crate) use mint::*;
 pub(crate) use transfer::*;
