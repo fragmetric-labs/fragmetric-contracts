807cdb8c98bce80ae72bc61a79b538b17b09cdc6
Kangji <drcotr0623@naver.com>
2024-08-05 15:11:28 +0900

    feat: implement fund events

--- programs/restaking/src/fund/events/mod.rs
+++ programs/restaking/src/fund/events/mod.rs
@@ -3,88 +3,132 @@ use anchor_lang::prelude::*;
 use crate::TokenInfo;
 
 #[event]
-pub struct FundInfoEvent {
+pub struct FundInfo {
     pub admin: Pubkey,
-    pub receipt_token_mint: Pubkey,
-    pub default_token_fee_rate: u16,
-    pub whitelisted_tokens: Vec<TokenInfo>,
+    pub lrt_mint: Pubkey,
+    pub supported_tokens: Vec<TokenInfo>,
     pub sol_amount_in: u128,
+    pub sol_reserved_amount: u128,
+    pub sol_withdrawal_fee_rate: f32,
+    pub sol_withdrawal_enabled: bool,
 }
 
-/// Split into `FundDepositSOLEvent` / `FundDepositTokenEvent`??
 #[event]
-pub struct FundDepositEvent {
-    /// redundant info?
-    pub is_token: bool,
-    pub token_mint: Option<Pubkey>,
-    pub receipt_token_mint: Pubkey,
+pub struct FundSOLDeposited {
+    pub user: Pubkey,
+    pub user_minted_lrt_account: Pubkey,
+
     pub sol_deposit_amount: u64,
     pub sol_amount_in_fund: u128,
-    pub token_deposit_amount: Option<u64>,
-    pub token_amount_in_fund: Option<u128>,
-    pub receipt_token_mint_amount: u64,
+    pub minted_lrt_mint: Pubkey,
+    pub minted_lrt_amount: u64,
+
+    // Not Implemented Yet: Always `None` for now
+    pub wallet_provider: Option<String>,
+    pub fpoint_accrual_rate_multiplier: Option<f32>,
+
+    pub fund_info: FundInfo,
 }
 
-/// When user requested withdrawal
 #[event]
-pub struct FundRequestWithdrawalEvent {
-    /// `user` vs. `user_wallet`??
-    pub user_wallet: Pubkey,
-    pub user_account: Pubkey,
-    pub receipt_token_mint: Pubkey,
-    pub batch_id: u64,
-    pub request_id: u64,
-    pub receipt_token_amount: u64,
-    pub timestamp: i64,
+pub struct FundTokenDeposited {
+    pub user: Pubkey,
+    pub user_minted_lrt_account: Pubkey,
+
+    pub deposited_token_mint: Pubkey,
+    pub deposited_token_user_account: Pubkey,
+
+    pub token_deposit_amount: u64,
+    pub token_amount_in_fund: u128,
+    pub minted_lrt_mint: Pubkey,
+    pub minted_lrt_amount: u64,
+
+    // Not Implemented Yet: Always `None` for now
+    pub wallet_provider: Option<String>,
+    pub fpoint_accrual_rate_multiplier: Option<f32>,
+
+    pub fund_info: FundInfo,
 }
 
-/// When user withdrawed SOL
 #[event]
-pub struct FundWithdrawEvent {
-    pub user_wallet: Pubkey,
-    pub user_account: Pubkey,
-    pub receipt_token_mint: Pubkey,
-    pub batch_id: u64,
+pub struct LRTTransferred {
+    pub lrt_mint: Pubkey,
+    pub lrt_amount: Pubkey,
+    pub source_lrt_account: Pubkey,
+    pub destination_lrt_account: Pubkey,
+}
+
+#[event]
+pub struct FundWithdrawalRequested {
+    pub user: Pubkey,
+    pub user_receipt_account: Pubkey,   // Receipt of withdrawal request
+
     pub request_id: u64,
-    pub receipt_token_amount: u64,
+    pub lrt_mint: Pubkey,
+    pub lrt_amount: u64,
+}
+
+#[event]
+pub struct FundWithdrawalRequestCancelled {
+    pub user: Pubkey,
+    pub user_receipt_account: Pubkey,   // Receipt of withdrawal request
+    
+    pub request_id: u64,
+    pub lrt_mint: Pubkey,
+    pub lrt_amount: u64,
+}
+
+#[event]
+pub struct FundSOLWithdrawed {
+    pub user: Pubkey,
+    pub user_receipt_account: Pubkey,   // Receipt of withdrawal request
+    pub user_minted_lrt_account: Pubkey,
+
+    pub request_id: u64,
+    pub lrt_mint: Pubkey,
+    pub lrt_amount: u64,
+
     pub sol_withdraw_amount: u64,
-    pub sol_remaining_in_reserved_fund: u128,
+    pub sol_fee_amount: u64,
+
+    pub fund_info: FundInfo,
 }
 
-/// When operator started processing pending batch
-///
-/// Can we merge `FundStartBatchWithdrawalEvent`, `FundProcessBatchWithdrawalEvent`,
-/// and `FundEndBatchWithdrawalEvent` into a single event??
-#[event]
-pub struct FundStartBatchWithdrawalEvent {
-    pub receipt_token_mint: Pubkey,
-    pub batch_id: u64,
-    pub num_withdrawal_requests: u64,
-    pub receipt_token_to_process: u128,
-}
+// // TO BE DONE
+// /// When operator started processing pending batch
+// ///
+// /// Can we merge `FundStartBatchWithdrawalEvent`, `FundProcessBatchWithdrawalEvent`,
+// /// and `FundEndBatchWithdrawalEvent` into a single event??
+// #[event]
+// pub struct FundStartBatchWithdrawalEvent {
+//     pub receipt_token_mint: Pubkey,
+//     pub batch_id: u64,
+//     pub num_withdrawal_requests: u64,
+//     pub receipt_token_to_process: u128,
+// }
 
-/// When operator partially processed the batch in progress
-#[event]
-pub struct FundProcessBatchWithdrawalEvent {
-    pub receipt_token_mint: Pubkey,
-    pub batch_id: u64,
-    pub num_withdrawal_requests: u64,
-    pub receipt_token_to_process: u128,
-    pub receipt_token_being_processed: u128,
-    pub receipt_token_processed: u128,
-    pub sol_reserved: u128,
-}
+// /// When operator partially processed the batch in progress
+// #[event]
+// pub struct FundProcessBatchWithdrawalEvent {
+//     pub receipt_token_mint: Pubkey,
+//     pub batch_id: u64,
+//     pub num_withdrawal_requests: u64,
+//     pub receipt_token_to_process: u128,
+//     pub receipt_token_being_processed: u128,
+//     pub receipt_token_processed: u128,
+//     pub sol_reserved: u128,
+// }
 
-/// When operator ended processing the batch in progress
-#[event]
-pub struct FundEndBatchWithdrawalEvent {
-    pub receipt_token_mint: Pubkey,
-    pub batch_id: u64,
-    pub num_withdrawal_requests_processed: u64,
-    pub total_withdrawal_requests_processed: u64,
-    pub receipt_token_processed: u128,
-    pub total_receipt_token_processed: u128,
-    pub sol_reserved: u128,
-    pub total_sol_reserved: u128,
-    pub sol_remaining: u128,
-}
+// /// When operator ended processing the batch in progress
+// #[event]
+// pub struct FundEndBatchWithdrawalEvent {
+//     pub receipt_token_mint: Pubkey,
+//     pub batch_id: u64,
+//     pub num_withdrawal_requests_processed: u64,
+//     pub total_withdrawal_requests_processed: u64,
+//     pub receipt_token_processed: u128,
+//     pub total_receipt_token_processed: u128,
+//     pub sol_reserved: u128,
+//     pub total_sol_reserved: u128,
+//     pub sol_remaining: u128,
+// }
