75ef1d0bd231bd4994af9284440d803723afd4b6
Dong Wook Kim <dehypnosis@gmail.com>
2024-09-10 11:43:02 +0900

    chore: add mainnet/devnet program id constants to idl

--- programs/restaking/src/constants/common.rs
+++ programs/restaking/src/constants/common.rs
@@ -1,6 +1,11 @@
 #![allow(dead_code, unused_imports)]
 use anchor_lang::prelude::*;
 
+#[constant]
+pub const MAINNET_PROGRAM_ID: Pubkey = pubkey!("fragnAis7Bp6FTsMoa6YcH8UffhEw43Ph79qAiK3iF3");
+#[constant]
+pub const DEVNET_PROGRAM_ID: Pubkey = pubkey!("frag9zfFME5u1SNhUYGa4cXLzMKgZXF3xwZ2Y1KCYTQ");
+
 /**
     Below address are needed to be passed to transactions which includes pricing of tokens (token deposit, withdrawal request)
     A complete list will be provided to client via address lookup table later.
