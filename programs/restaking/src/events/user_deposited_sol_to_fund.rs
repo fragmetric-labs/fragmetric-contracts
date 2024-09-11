21a01e2d75ed747d8a0234c07104c7bef6e187a5
Kangji <drcotr0623@naver.com>
2024-09-11 12:52:14 +0900

    fix: set expandible type as last field

--- programs/restaking/src/events/user_deposited_sol_to_fund.rs
+++ programs/restaking/src/events/user_deposited_sol_to_fund.rs
@@ -5,7 +5,6 @@ use crate::modules::fund::{FundAccountInfo, UserFundAccount};
 #[event]
 pub struct UserDepositedSOLToFund {
     pub receipt_token_mint: Pubkey,
-    pub fund_account: FundAccountInfo,
 
     pub user: Pubkey,
     pub user_receipt_token_account: Pubkey,
@@ -15,4 +14,6 @@ pub struct UserDepositedSOLToFund {
     pub contribution_accrual_rate: Option<u8>, // 100 is 1.0
     pub deposited_sol_amount: u64,
     pub minted_receipt_token_amount: u64,
+
+    pub fund_account: FundAccountInfo,
 }
