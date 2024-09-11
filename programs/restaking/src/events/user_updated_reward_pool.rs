442709d2ce3e432a1a7c9165f85166bdbf397ab5
Kangji <drcotr0623@naver.com>
2024-09-11 15:00:27 +0900

    fix: add user reward update event on initialization

--- programs/restaking/src/events/user_updated_reward_pool.rs
+++ programs/restaking/src/events/user_updated_reward_pool.rs
@@ -17,13 +17,13 @@ impl UserUpdatedRewardPool {
         }
     }
 
-    // pub fn new_from_initialize(
-    //     receipt_token_mint: Pubkey,
-    //     user_reward_account_address: Pubkey,
-    // ) -> Self {
-    //     Self {
-    //         receipt_token_mint,
-    //         updated_user_reward_account_addresses: vec![user_reward_account_address],
-    //     }
-    // }
+    pub fn new_from_initialize(
+        receipt_token_mint: Pubkey,
+        user_reward_account_address: Pubkey,
+    ) -> Self {
+        Self {
+            receipt_token_mint,
+            updated_user_reward_account_addresses: vec![user_reward_account_address],
+        }
+    }
 }
