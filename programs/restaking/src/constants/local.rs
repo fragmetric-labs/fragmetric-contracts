acc9327d9223ad6f58953ef07e758147b1b94ed9
taehoon <kth7024@naver.com>
2024-10-30 14:16:15 +0900

    fix: refactor deposit context struct

--- programs/restaking/src/constants/local.rs
+++ programs/restaking/src/constants/local.rs
@@ -16,3 +16,18 @@ pub const FUND_MANAGER_PUBKEY: Pubkey = pubkey!(/*local:FUND_MANAGER*/"5FjrErTQ9
 
 #[constant]
 pub const FRAGSOL_MINT_ADDRESS: Pubkey = pubkey!(/*local:FRAGSOL_MINT*/"Cs29UiPhAkM2v8fZW7qCJ1UjhF1UAhgrsKj61yGGYizD"/**/);
+
+#[constant]
+pub const JITO_VAULT_PROGRAM_ID: Pubkey = pubkey!("34X2uqBhEGiWHu43RDEMwrMqXF4CpCPEZNaKdAaUS9jx");
+
+#[constant]
+pub const JITO_VAULT_CONFIG_ADDRESS: Pubkey = pubkey!("Cx2tQmB4RdCQADK8dGzt9sbXDpQ9o2pcjuhKnN42NxbK");
+
+#[constant]
+pub const JITO_VAULT_ADDRESS: Pubkey = pubkey!("8bCy6TWfxc7H2ib61ijR1LzGynZNuVspdeUNra9AS9Lg");
+
+#[constant]
+pub const JITO_VAULT_RECEIPT_TOKEN: Pubkey = pubkey!("5w2JCmAbBdSRv1y8igM3YNjvnGdfYUeYuVmtw9fU5TXZ");
+
+#[constant]
+pub const JITO_VAULT_SUPPORTED_TOKEN: Pubkey = pubkey!("J1shGRurZVzL2DqZNzfSU8s44H2B94kw5YyyckuszG1N");
