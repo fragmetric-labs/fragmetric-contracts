235fe1e2ea6a074d13bc6acd3bf6489ed9debc70
taehoon <kth7024@naver.com>
2024-12-09 22:11:48 +0900

    fix: remove test

--- tests/restaking/9_operator_restaking.ts
+++ tests/restaking/9_operator_restaking.ts
@@ -41,7 +41,6 @@ describe("operator_restake_with_normalize", async () => {
 
     step("request VRT from jito restaking pool", async function () {
         const unrestakeSolAmount = new BN(4 * web3.LAMPORTS_PER_SOL);
-
         await restaking.runOperatorRun({
                 command: {
                     unrestakeVrt: {
@@ -65,74 +64,5 @@ describe("operator_restake_with_normalize", async () => {
             100,
             3_000_000,
         );
-    });
-    step("request VRT from jito restaking pool2", async function () {
-        const unrestakeSolAmount = new BN(1 * web3.LAMPORTS_PER_SOL);
-
-        await restaking.runOperatorRun({
-                command: {
-                    unrestakeVrt: {
-                        0: {
-                            items: [
-                                {
-                                    vaultAddress: restaking.knownAddress.fragSOLJitoVaultAccount,
-                                    solAmount: unrestakeSolAmount,
-                                },
-                            ],
-                            state: {
-                                init: {},
-                            },
-                        }
-                    },
-                },
-                requiredAccounts: [],
-
-            },
-            restaking.keychain.getKeypair('FUND_MANAGER'),
-            100,
-            3_000_000,
-        );
-    });
-
-    step("claim VRT from jito restaking pool", async function () {
-        let currentSlot = await restaking.connection.getSlot();
-        let afterSlot: number;
-        console.log("waiting for cooling down")
-        do {
-            await new Promise(r => setTimeout(r, 20 * 1000))
-            afterSlot = await restaking.connection.getSlot();
-        } while (Math.floor((afterSlot - currentSlot) / 32) < 2);
-        console.log("start claim vrt")
-
-        const unrestakeSolAmount = new BN(4 * web3.LAMPORTS_PER_SOL);
-
-        await restaking.runOperatorRun({
-                command: {
-                    claimUnrestakedVst: {
-                        0: {
-                            items: [
-                                {
-                                    vaultAddress: restaking.knownAddress.fragSOLJitoVaultAccount,
-                                },
-                            ],
-                            state: {
-                                init: {},
-                            },
-                        }
-                    },
-                },
-                requiredAccounts: [],
-
-            },
-            restaking.keychain.getKeypair('FUND_MANAGER'),
-            100,
-            3_000_000,
-        );
-
-        let fundAccount = await restaking.getFragSOLFundAccount()
-        if (fundAccount.operation.nextCommand == null) {
-
-        }
-
     });
 });
