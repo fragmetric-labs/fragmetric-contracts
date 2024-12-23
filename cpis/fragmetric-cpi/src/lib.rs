c3d729c8bbfacdb7f51a8ab6ee31b72128ff92ee
Kangji <drcotr0623@naver.com>
2024-12-23 21:23:54 +0900

    feat: add devnet idl

--- cpis/fragmetric-cpi/src/lib.rs
+++ cpis/fragmetric-cpi/src/lib.rs
@@ -1,1 +1,16 @@
-anchor_lang::declare_program!(fragmetric);
+#[doc(hidden)]
+#[cfg(feature = "devnet")]
+mod __devnet {
+    anchor_lang::declare_program!(fragmetric_devnet);
+}
+
+#[doc(hidden)]
+#[cfg(not(feature = "devnet"))]
+mod __mainnet {
+    anchor_lang::declare_program!(fragmetric);
+}
+
+#[cfg(feature = "devnet")]
+pub use __devnet::fragmetric_devnet as fragmetric;
+#[cfg(not(feature = "devnet"))]
+pub use __mainnet::fragmetric;
