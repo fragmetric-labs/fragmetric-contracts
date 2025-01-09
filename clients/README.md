6af8d5a859b76ce8c8b98b3046dd16e3a9c4a571
Daniel Kim <dehypnosis@gmail.com>
2025-01-09 14:01:46 +0900

    playground scaffold

--- clients/README.md
+++ clients/README.md
@@ -1,40 +1,13 @@
-# Tools
+# Client Libraries
 
-## 1. JavaScript Client SDK
-
-### How to contribute?
-```
-# dev/build/test SDK
-$ yarn workspace @fragmetric-labs/sdk dev
-$ yarn workspace @fragmetric-labs/sdk dev:node # build only node:cjs, node:esm
-$ yarn workspace @fragmetric-labs/sdk dev:node:cjs # fastest for nodejs playground testing
-$ yarn workspace @fragmetric-labs/sdk dev:browser # build only web:cjs, web:esm, web:umd
-$ yarn workspace @fragmetric-labs/sdk dev:browser:esm # fastest for browser sdk testing
-$ yarn workspace @fragmetric-labs/sdk build
-$ yarn workspace @fragmetric-labs/sdk test
-
-# dev/build/test examples
-$ yarn workspace @fragmetric-labs/sdk-example-react dev
-$ yarn workspace @fragmetric-labs/sdk-example-html dev
-$ yarn workspace @fragmetric-labs/sdk-example-node dev
-
-# build all including examples
-$ yarn workspaces run build
-
-# test all including examples
-$ yarn workspaces run test
-
-# publish sdk
-$ yarn workspace @fragmetric-labs/sdk publish
-```
+### 1. JavaScript SDK
+...
 
 ## 2. Node.js Playground
+...
 
-### ...
-
-## 3. Rust Crates for CPI
-
-### ...
+## 3. Rust Crates
+...
 
 
 # TODO
@@ -46,7 +19,6 @@ $ yarn workspace @fragmetric-labs/sdk publish
 - playground(? .. = REPL + wrapper functions (?) for testing and operation? or just into sdk?)
 - fragSOL, fragJTO playgrounds...
 - snapshot feature for some stuffs.. for fixture based testing
-- testing/playground migration...
-- update keychain stuff.. and aws secret.. readme..
-- migrate `restaking`, `lib`, `../keypairs`, `../idls`, `../cpis`
+- migrate keychain stuff.. and aws secret.. readme..
+- migrate `tools`, `keypairs`
 - rewrite `tests`
---
+++ clients/js/playground/index.ts
@@ -0,0 +1,62 @@
+import * as web3 from '@solana/web3.js';
+import { RestakingProgram, LedgerSigner } from '@fragmetric-labs/sdk';
+
+
+(async function example1() {
+    const walletKeyPair = web3.Keypair.fromSecretKey(Uint8Array.from([18,99,108,102,2,206,6,7,168,174,190,163,59,172,204,141,105,14,181,146,108,161,134,128,169,57,152,205,238,237,220,216,150,75,239,172,33,80,166,64,55,49,182,185,30,49,104,33,14,163,68,64,59,209,64,244,34,15,83,110,17,139,78,4]));
+    const ledgerPublicKey = new web3.PublicKey("79AHDsvEiM4MNrv8GPysgiGPj1ZPmxviF3dw29akYC84");
+
+    const program = new RestakingProgram({
+        cluster: 'devnet',
+        connection: undefined, // default RPC
+        receiptTokenMint: 'fragSOL',
+        transactionHandler: {
+            onBeforeSign: async (txMessage) => {
+                console.log(`[signing] description: ${txMessage.descriptions?.join(', ')}`);
+            },
+            onSign: async (tx, publicKey, name) => {
+                if (publicKey.equals(walletKeyPair.publicKey)) {
+
+                    console.log(`[signed] by keypair: ${publicKey}`);
+                    return walletKeyPair;
+                } else if (publicKey.equals(ledgerPublicKey)) {
+                    const ledger = await LedgerSigner.connect({
+                        handler: {
+                            onBeforeConnect: (bip32Path) => console.log(`[ledger] connecting: ${bip32Path}`),
+                            onConnect: (publicKey, solanaAppVersion) => console.log(`[ledger] connected: ${publicKey} (solana app version: ${solanaAppVersion})`),
+                            onError: (err) => {
+                                console.error(err);
+                                return true; // always retry
+                            },
+                        },
+                    });
+
+                    console.log(`[ledger] signing: ${publicKey}`);
+                    const res = await ledger.signTransaction(tx);
+
+                    console.log(`[signed] by ledger: ${publicKey}`);
+                    return res;
+                }
+                return null;
+            },
+            onBeforeConfirm: async (tx, confirmStrategy, commitment) => {
+                console.log(`[confirming] commitment: ${commitment}`, confirmStrategy);
+            },
+            onConfirm: async (result) => {
+                console.log(`[confirmed] signature: ${result.signature}`, result.events);
+            },
+        },
+    });
+
+    const res1 = await program
+        .operator
+        .updateFundPrices({ operator: walletKeyPair.publicKey })
+        .then(txMessage => txMessage.send());
+    console.log(res1);
+
+    const res2 = await program
+        .operator
+        .updateFundPrices({ operator: ledgerPublicKey })
+        .then(txMessage => txMessage.send());
+    console.log(res2);
+})();
