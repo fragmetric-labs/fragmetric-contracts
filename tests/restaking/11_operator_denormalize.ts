424f719b54b9eef882424c5834fbea9eb250af03
Jung Hyun Chun <ouoabcde@naver.com>
2025-01-27 06:05:15 +0900

    feat: add denormalize_nt command (#248) (#254)

---
+++ tests/restaking/11_operator_denormalize.ts
@@ -0,0 +1,42 @@
+import {BN, web3} from '@coral-xyz/anchor';
+import {expect} from "chai";
+import {step} from "mocha-steps";
+import {restakingPlayground} from "../restaking";
+
+describe("operator_denormalize", async () => {
+    const restaking = await restakingPlayground;
+
+    step("normalize", async function () {
+        await restaking.runOperatorFundCommands({
+                command: {
+                    normalizeSt: {
+                        0: {
+                            state: {
+                                new: {},
+                            },
+                        }
+                    },
+                },
+                requiredAccounts: [],
+            },
+            restaking.keychain.getKeypair("FUND_MANAGER"),
+        );
+    });
+
+    step("denormalize", async function() {
+        await restaking.runOperatorFundCommands({
+                command: {
+                    denormalizeNt: {
+                        0: {
+                            state: {
+                                new: {},
+                            },
+                        }
+                    },
+                },
+                requiredAccounts: [],
+            },
+            restaking.keychain.getKeypair("FUND_MANAGER"),
+        );
+    });
+});
