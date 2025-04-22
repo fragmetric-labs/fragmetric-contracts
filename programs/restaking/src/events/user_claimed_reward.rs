8871881ca23e8f4abc452464708f7366221029b8
Kangji <drcotr0623@naver.com>
2025-04-22 18:11:51 +0900

    fix(reward): add destination token account owner in claim event

--- programs/restaking/src/events/user_claimed_reward.rs
+++ programs/restaking/src/events/user_claimed_reward.rs
@@ -5,6 +5,7 @@ pub struct UserClaimedReward {
     pub receipt_token_mint: Pubkey,
     pub reward_token_mint: Pubkey,
     pub destination_reward_token_account: Pubkey,
+    pub destination_reward_token_account_owner: Pubkey,
     pub updated_reward_account: Pubkey,
     pub updated_user_reward_account: Pubkey,
     pub claimed_reward_token_amount: u64,
