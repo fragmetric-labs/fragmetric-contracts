9af3554bff22eca62356c318f85a1eb5dde0cff6
Terry Kang <maple19out@naver.com>
2025-04-23 17:45:16 +0900

    feat: changed remove logic(shift -> swap)

--- programs/restaking/src/modules/fund/fund_account_restaking_vault.rs
+++ programs/restaking/src/modules/fund/fund_account_restaking_vault.rs
@@ -153,11 +153,11 @@ impl RestakingVault {
             .position(|reward_token| *reward_token == compounding_reward_token_mint)
             .ok_or(ErrorCode::FundRestakingVaultCompoundingRewardTokenNotRegisteredError)?;
 
-        // shift left starting from matched_idx
-        for idx in matched_idx..self.num_compounding_reward_tokens as usize - 1 {
-            self.compounding_reward_token_mints[idx] = self.compounding_reward_token_mints[idx + 1];
-        }
         self.num_compounding_reward_tokens -= 1;
+        self.compounding_reward_token_mints
+            .swap(matched_idx, self.num_compounding_reward_tokens as usize);
+        self.compounding_reward_token_mints[self.num_compounding_reward_tokens as usize] =
+            Pubkey::default();
 
         Ok(())
     }
@@ -199,12 +199,11 @@ impl RestakingVault {
             .position(|reward_token| *reward_token == distributing_reward_token_mint)
             .ok_or(ErrorCode::FundRestakingVaultDistributingRewardTokenNotRegisteredError)?;
 
-        // shift left starting from matched_idx
-        for idx in matched_idx..self.num_distributing_reward_tokens as usize - 1 {
-            self.distributing_reward_token_mints[idx] =
-                self.distributing_reward_token_mints[idx + 1];
-        }
         self.num_distributing_reward_tokens -= 1;
+        self.distributing_reward_token_mints
+            .swap(matched_idx, self.num_distributing_reward_tokens as usize);
+        self.distributing_reward_token_mints[self.num_distributing_reward_tokens as usize] =
+            Pubkey::default();
 
         Ok(())
     }
