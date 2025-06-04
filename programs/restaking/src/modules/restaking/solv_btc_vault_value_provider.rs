8f98123c5ff3166858c0d6ba38d8ddd823b29dfd
Daniel Kim <dehypnosis@gmail.com>
2025-06-04 09:51:03 +0900

    fix(restaking/solv): fixed solv vault value provider

--- programs/restaking/src/modules/restaking/solv_btc_vault_value_provider.rs
+++ programs/restaking/src/modules/restaking/solv_btc_vault_value_provider.rs
@@ -18,7 +18,7 @@ impl TokenValueProvider for SolvBTCVaultValueProvider {
         let vault_loader = AccountLoader::<VaultAccount>::try_from(pricing_source_accounts[0])?;
         let vault = vault_loader.load()?;
 
-        require_keys_eq!(vault.get_vst_mint(), *token_mint);
+        require_keys_eq!(vault.get_vrt_mint(), *token_mint);
 
         result.numerator.clear();
         result.numerator.reserve_exact(1);
