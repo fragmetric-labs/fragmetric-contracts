bf7b74da3b2d2dbf530e521ca169091334ac3dd4
Kangji <drcotr0623@naver.com>
2025-06-24 13:12:37 +0900

    chore(restaking/swap): cleanup and lint

--- programs/restaking/src/modules/swap/mod.rs
+++ programs/restaking/src/modules/swap/mod.rs
@@ -2,6 +2,7 @@ mod orca_dex_liquidity_pool_service;
 mod orca_dex_liquidity_pool_value_provider;
 mod token_swap_source;
 
+#[allow(unused_imports)]
 pub use orca_dex_liquidity_pool_service::*;
 pub use orca_dex_liquidity_pool_value_provider::*;
 pub use token_swap_source::*;
@@ -53,6 +54,7 @@ pub(in crate::modules) fn validate_swap_source<'info>(
     from_token_mint: &InterfaceAccount<Mint>,
     to_token_mint: &InterfaceAccount<Mint>,
 ) -> Result<()> {
+    #[deny(clippy::wildcard_enum_match_arm)]
     match swap_source {
         TokenSwapSource::OrcaDEXLiquidityPool { address } => {
             require_keys_eq!(*address, swap_source_account.key());
