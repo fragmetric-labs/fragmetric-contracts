43f9bad7f3df06a7344483f915953b5df8005ded
Kangji <drcotr0623@naver.com>
2025-07-15 18:15:20 +0900

    revert "fix(staking/spl): change stake account rent calculation" to see if test fails
    This reverts commit 347e708b7e95406dfcd80ea59a4039ec29aa712f.

--- programs/restaking/src/modules/staking/spl_stake_pool_service.rs
+++ programs/restaking/src/modules/staking/spl_stake_pool_service.rs
@@ -639,6 +639,8 @@ impl<'info, T: SPLStakePoolInterface> SPLStakePoolService<'info, T> {
             return Ok((0, 0, 0));
         }
 
+        let payer_lamports_before = to_stake_account_rent_payer.lamports();
+
         // initialize `to_stake_account` first - will be used for split stake
         system_program.initialize_account(
             to_stake_account,
@@ -649,7 +651,7 @@ impl<'info, T: SPLStakePoolInterface> SPLStakePoolService<'info, T> {
             &solana_program::stake::program::ID,
         )?;
 
-        let rent = to_stake_account.lamports();
+        let rent_payed = payer_lamports_before - to_stake_account_rent_payer.lamports();
 
         let withdraw_stake_ix = spl_stake_pool::instruction::withdraw_stake(
             self.spl_stake_pool_program.key,
@@ -687,7 +689,7 @@ impl<'info, T: SPLStakePoolInterface> SPLStakePoolService<'info, T> {
             from_pool_token_account_signer_seeds,
         )?;
 
-        let unstaking_sol_amount = to_stake_account.lamports().saturating_sub(rent);
+        let unstaking_sol_amount = to_stake_account.lamports().saturating_sub(rent_payed);
 
         // deactivate `to_stake_account` - since it's state is active now as
         // it has been splitted from active stake account
