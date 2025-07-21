46dee706a767f4ade3893a36e7d0459d242b5116
Terry Kang <maple19out@naver.com>
2025-07-21 09:15:01 +0900

    refactor(restaking): remove unused constants

--- programs/restaking/src/constants/devnet.rs
+++ programs/restaking/src/constants/devnet.rs
@@ -18,70 +18,8 @@ pub const PROGRAM_REVENUE_ADDRESS: Pubkey = pubkey!("SRCMj3B7cYjvwTtqJxUSptgJPWk
 pub const JITO_VAULT_PROGRAM_ID: Pubkey = pubkey!("Vau1t6sLNxnzB7ZDsef8TLbPLfyZMYXH8WTNqUdm9g8");
 
 #[constant]
-pub const JITO_VAULT_PROGRAM_FEE_WALLET: Pubkey =
-    pubkey!("9eZbWiHsPRsxLSiHxzg2pkXsAuQMwAjQrda7C7e21Fw6");
-
-#[constant]
 pub const JITO_VAULT_CONFIG_ADDRESS: Pubkey =
     pubkey!("UwuSgAq4zByffCGCrWH87DsjfsewYjuqHfJEpzw1Jq3");
 
 #[constant]
 pub const SOLV_PROGRAM_ID: Pubkey = pubkey!("FsoLVPaSSXfsfZHYaxtQSZs6npFFUeRCyXB6dcC8ckn");
-
-////////////////////////////////////////////
-// receipt token: fragSOL
-////////////////////////////////////////////
-
-#[constant]
-pub const FRAGSOL_ADDRESS_LOOKUP_TABLE_ADDRESS: Pubkey =
-    pubkey!("5i5ExdTT7j36gKyiyjhaEcqFWUESvi6maASJyxKVZLyU");
-
-#[constant]
-pub const FRAGSOL_MINT_ADDRESS: Pubkey = pubkey!("FRAGSEthVFL7fdqM8hxfxkfCZzUvmg21cqPJVvC1qdbo");
-
-#[constant]
-pub const FRAGSOL_NORMALIZED_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("nSoLnkrvh2aY792pgCNT6hzx84vYtkviRzxvhf3ws8e");
-
-#[constant]
-pub const FRAGSOL_WRAPPED_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U");
-
-#[constant]
-pub const FRAGSOL_JITO_NSOL_VAULT_ACCOUNT_ADDRESS: Pubkey =
-    pubkey!("BxhsigZDYjWTzXGgem9W3DsvJgFpEK5pM2RANP22bxBE");
-
-#[constant]
-pub const FRAGSOL_JITO_NSOL_VAULT_RECEIPT_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("7jff6VT8twUX3513HuhN7EF18DtUzBj2N1goWroZ29t");
-
-#[constant]
-pub const FRAGSOL_JITO_JITOSOL_VAULT_ACCOUNT_ADDRESS: Pubkey =
-    pubkey!("2KeeztiFcCV6HpBHrWYyuv8hYrhu27imm6XgknaM7NNG");
-
-#[constant]
-pub const FRAGSOL_JITO_JITOSOL_VAULT_RECEIPT_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("FRj1uf9W7TsGfSoHc1gU6V2sKxs4wMhaXC5A8KjVYvA5");
-
-////////////////////////////////////////////
-// receipt token: fragJTO
-////////////////////////////////////////////
-
-#[constant]
-pub const FRAGJTO_ADDRESS_LOOKUP_TABLE_ADDRESS: Pubkey =
-    pubkey!("6VHmiiuZAW2PVoY5N16oqs8wYVkXnfmZBcM7Vkbb76jH");
-
-#[constant]
-pub const FRAGJTO_MINT_ADDRESS: Pubkey = pubkey!("FRAGJ157KSDfGvBJtCSrsTWUqFnZhrw4aC8N8LqHuoos");
-
-#[constant]
-pub const FRAGJTO_WRAPPED_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("WFRGJnQt5pK8Dv4cDAbrSsgPcmboysrmX3RYhmRRyTR");
-
-#[constant]
-pub const FRAGJTO_JITO_JTO_VAULT_ACCOUNT_ADDRESS: Pubkey =
-    pubkey!("7dCQpU5w6Xz3aAnpFrXByBg9LxLdz33deUCrWJAVcNaE");
-
-#[constant]
-pub const FRAGJTO_JITO_JTO_VAULT_RECEIPT_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("6VSjoP9hyHKKNZfcDzrAKRKWKSnyKhzLgBR9dtewPN9z");
--- programs/restaking/src/constants/mainnet.rs
+++ programs/restaking/src/constants/mainnet.rs
@@ -18,70 +18,8 @@ pub const PROGRAM_REVENUE_ADDRESS: Pubkey = pubkey!("XEhpR3UauMkARQ8ztwaU9Kbv16j
 pub const JITO_VAULT_PROGRAM_ID: Pubkey = pubkey!("Vau1t6sLNxnzB7ZDsef8TLbPLfyZMYXH8WTNqUdm9g8");
 
 #[constant]
-pub const JITO_VAULT_PROGRAM_FEE_WALLET: Pubkey =
-    pubkey!("5eosrve6LktMZgVNszYzebgmmC7BjLK8NoWyRQtcmGTF");
-
-#[constant]
 pub const JITO_VAULT_CONFIG_ADDRESS: Pubkey =
     pubkey!("UwuSgAq4zByffCGCrWH87DsjfsewYjuqHfJEpzw1Jq3");
 
 #[constant]
 pub const SOLV_PROGRAM_ID: Pubkey = pubkey!("FSoLvf9dv17a4DzMGYKxqFnDGj9EiXRW5wKrwQ39UDH");
-
-////////////////////////////////////////////
-// receipt token: fragSOL
-////////////////////////////////////////////
-
-#[constant]
-pub const FRAGSOL_ADDRESS_LOOKUP_TABLE_ADDRESS: Pubkey =
-    pubkey!("HjNXH2HMfso5YU6U7McfhsbfoecGR5QTBAxTCSbFoYqy");
-
-#[constant]
-pub const FRAGSOL_MINT_ADDRESS: Pubkey = pubkey!("FRAGSEthVFL7fdqM8hxfxkfCZzUvmg21cqPJVvC1qdbo");
-
-#[constant]
-pub const FRAGSOL_NORMALIZED_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("nSoLnkrvh2aY792pgCNT6hzx84vYtkviRzxvhf3ws8e");
-
-#[constant]
-pub const FRAGSOL_WRAPPED_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U");
-
-#[constant]
-pub const FRAGSOL_JITO_NSOL_VAULT_ACCOUNT_ADDRESS: Pubkey =
-    pubkey!("HR1ANmDHjaEhknvsTaK48M5xZtbBiwNdXM5NTiWhAb4S");
-
-#[constant]
-pub const FRAGSOL_JITO_NSOL_VAULT_RECEIPT_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("CkXLPfDG3cDawtUvnztq99HdGoQWhJceBZxqKYL2TUrg");
-
-#[constant]
-pub const FRAGSOL_JITO_JITOSOL_VAULT_ACCOUNT_ADDRESS: Pubkey =
-    pubkey!("CyGwYsiNg1FEYNCVXmPAixCkn2ZqWQVQJVy9RBCPNF7t");
-
-#[constant]
-pub const FRAGSOL_JITO_JITOSOL_VAULT_RECEIPT_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("FRj1uf9W7TsGfSoHc1gU6V2sKxs4wMhaXC5A8KjVYvA5");
-
-////////////////////////////////////////////
-// receipt token: fragJTO
-////////////////////////////////////////////
-
-#[constant]
-pub const FRAGJTO_ADDRESS_LOOKUP_TABLE_ADDRESS: Pubkey =
-    pubkey!("AQtDes99nLUnSK6BQJgj9KJ6b3eDv8bUUxGCmnEJUkY5");
-
-#[constant]
-pub const FRAGJTO_MINT_ADDRESS: Pubkey = pubkey!("FRAGJ157KSDfGvBJtCSrsTWUqFnZhrw4aC8N8LqHuoos");
-
-#[constant]
-pub const FRAGJTO_WRAPPED_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("WFRGJnQt5pK8Dv4cDAbrSsgPcmboysrmX3RYhmRRyTR");
-
-#[constant]
-pub const FRAGJTO_JITO_JTO_VAULT_ACCOUNT_ADDRESS: Pubkey =
-    pubkey!("BmJvUzoiiNBRx3v2Gqsix9WvVtw8FaztrfBHQyqpMbTd");
-
-#[constant]
-pub const FRAGJTO_JITO_JTO_VAULT_RECEIPT_TOKEN_MINT_ADDRESS: Pubkey =
-    pubkey!("FRJtoBLuU72X3qgkVeBU1wXtmgQpWQmWptYsAdyyu3qT");
