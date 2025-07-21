9c1ea543e6727843b5f642d4c815e880ddd35c67
Terry Kang <maple19out@naver.com>
2025-07-21 14:51:37 +0900

    refactor: remove redundant attribute macro

--- programs/restaking/src/constants/local.rs
+++ programs/restaking/src/constants/local.rs
@@ -3,12 +3,9 @@ use anchor_lang::prelude::*;
 declare_id!("4qEHCzsLFUnw8jmhmRSmAK5VhZVoSD1iVqukAf92yHi5");
 
 // privileged for non-financial operations and scheduled tasks
-#[constant]
 pub const ADMIN_PUBKEY: Pubkey = pubkey!("9b2RSMDYskVvjVbwF4cVwEhZUaaaUgyYSxvESmnoS4LL");
 
 // privileged for financial operations and fund configuration
-#[constant]
 pub const FUND_MANAGER_PUBKEY: Pubkey = pubkey!("5FjrErTQ9P1ThYVdY9RamrPUCQGTMCcczUjH21iKzbwx");
 
-#[constant]
 pub const PROGRAM_REVENUE_ADDRESS: Pubkey = pubkey!("GuSruSKKCmAGuWMeMsiw3mbNhjeiRtNhnh9Eatgz33NA");
--- programs/solv/src/constants/devnet.rs
+++ programs/solv/src/constants/devnet.rs
@@ -2,11 +2,8 @@ use anchor_lang::prelude::*;
 
 declare_id!("FsoLVPaSSXfsfZHYaxtQSZs6npFFUeRCyXB6dcC8ckn");
 
-#[constant]
 pub const ZBTC_MINT_ADDRESS: Pubkey = pubkey!("FaKEZbaAE42h7aCSzzUMKP8woZYBXh43v5bPzqb8CyH");
 
-#[constant]
 pub const CBBTC_MINT_ADDRESS: Pubkey = pubkey!("FakEcBQk7MfreV3anJK6q136sPcieQ5dmmxXjaxfskGt");
 
-#[constant]
 pub const WBTC_MINT_ADDRESS: Pubkey = pubkey!("FaKEwBj5eNHg8en4Tv1YuQYUSjXnR9TZfVLaMsy3qv7s");
