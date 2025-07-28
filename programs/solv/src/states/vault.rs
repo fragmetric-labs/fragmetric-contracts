c3280ded606a990fbb0bd72906e9e3f0f142e00c
Minseok Kang <maple19out@naver.com>
2025-07-28 15:23:01 +0900

    test(solv): refine input relations (#485)
    * fix(solv/test): refine relationship between vst, srt amount during vault creation
    
    * fix(solv/test): consider srt operation reserved amount
    
    * fix(solv/test): replace saturating_sub to normal subtraction
    
    * fix(solv/test): adjust maximum possible token amounts

--- programs/solv/src/states/vault.rs
+++ programs/solv/src/states/vault.rs
@@ -1567,17 +1567,30 @@ mod tests {
 
     prop_compose! {
         fn vault()
-            (vrt_supply in 1..u64::MAX)
             (
-                vrt_supply in Just(vrt_supply),
-                extra_vst in 0..vrt_supply.min(u64::MAX - vrt_supply + 1),
+                vst_amount in 0..=BTC_MAX_SUPPLY,
                 one_srt_as_micro_vst in 100_000_000_000_000u64..200_000_000_000_000,
+            )
+            (
+                vst_amount in Just(vst_amount),
+                one_srt_as_micro_vst in Just(one_srt_as_micro_vst),
+                srt_amount in 0..=BTC_MAX_SUPPLY.min(
+                    (
+                        (BTC_MAX_SUPPLY - vst_amount) as u128 * 100_000_000_000_000 / one_srt_as_micro_vst as u128
+                    ) as u64
+                ),
             )
         -> VaultAccount {
             let mut vault = VaultAccount::dummy();
-            vault.vrt_supply = vrt_supply;
-            vault.vst_operation_reserved_amount = vrt_supply + extra_vst;
+
+            vault.vst_operation_reserved_amount = vst_amount;
+            vault.srt_operation_reserved_amount = srt_amount;
             vault.one_srt_as_micro_vst = one_srt_as_micro_vst;
+
+            let srt_amount_as_vst = vault.get_srt_exchange_rate().get_srt_amount_as_vst(srt_amount, false).unwrap();
+            vault.vrt_supply = vault.get_vrt_amount_to_mint(vst_amount + srt_amount_as_vst).unwrap();
+            vault.update_vrt_exchange_rate().unwrap();
+
             vault
         }
     }
@@ -1623,7 +1636,7 @@ mod tests {
     proptest! {
         #[test]
         fn test_initial_mint_vrt_with_vst(
-            vst_amount in 0..u64::MAX
+            vst_amount in 0..=BTC_MAX_SUPPLY
         ) {
             let mut vault = VaultAccount::dummy();
             let old_vault = vault.clone();
@@ -1648,9 +1661,9 @@ mod tests {
         #[test]
         fn test_mint_vrt_with_vst(
             mut vault in vault(),
-            mut vst_amount in 0..u64::MAX,
+            mut vst_amount in 0..=BTC_MAX_SUPPLY,
         ) {
-            vst_amount = vst_amount.min(u64::MAX - vault.vst_operation_reserved_amount);
+            vst_amount = vst_amount.min(BTC_MAX_SUPPLY - vault.vst_operation_reserved_amount);
             let old_vault = vault.clone();
 
             // VRT Price ≥ 1
@@ -1671,7 +1684,7 @@ mod tests {
         }
 
         #[test]
-        fn test_initial_mint_vrt_with_srt(srt_amount in 0..u64::MAX) {
+        fn test_initial_mint_vrt_with_srt(srt_amount in 0..=BTC_MAX_SUPPLY) {
             let mut vault = VaultAccount::dummy();
             let old_vault = vault.clone();
 
@@ -1698,7 +1711,7 @@ mod tests {
             mut srt_amount in 0..BTC_MAX_SUPPLY,
         ) {
             let nav = vault.get_net_asset_value_as_vst().unwrap();
-            srt_amount = srt_amount.min(BTC_MAX_SUPPLY.saturating_sub(nav));
+            srt_amount = srt_amount.min(BTC_MAX_SUPPLY - nav);
 
             let old_vault = vault.clone();
 
@@ -1897,9 +1910,12 @@ mod tests {
                 .unwrap();
             let old_vault = vault.clone();
 
+            let srt_operation_reserved_amount_as_vst = vault
+                .get_srt_exchange_rate()
+                .get_srt_amount_as_vst(vault.srt_operation_reserved_amount, false).unwrap();
             let expected_vrt_amount = div_util(
                 vault.vrt_supply as u128
-                    * (vault.vst_operation_reserved_amount + vault.vst_operation_receivable_amount)
+                    * (vault.vst_operation_reserved_amount + vault.vst_operation_receivable_amount + srt_operation_reserved_amount_as_vst)
                         as u128,
                 vault.get_net_asset_value_as_vst().unwrap(),
                 false,
@@ -1937,9 +1953,12 @@ mod tests {
                 .unwrap();
             let old_vault = vault.clone();
 
+            let srt_operation_reserved_amount_as_vst = vault
+                .get_srt_exchange_rate()
+                .get_srt_amount_as_vst(vault.srt_operation_reserved_amount, false).unwrap();
             let expected_vrt_amount = div_util(
                 vault.vrt_supply as u128
-                    * (vault.vst_operation_reserved_amount + vault.vst_operation_receivable_amount)
+                    * (vault.vst_operation_reserved_amount + vault.vst_operation_receivable_amount + srt_operation_reserved_amount_as_vst)
                         as u128,
                 vault.get_net_asset_value_as_vst().unwrap(),
                 false,
@@ -1963,8 +1982,12 @@ mod tests {
                 expected_vst_amount,
             );
 
+            let old_srt_operation_reserved_amount_as_vst = old_vault
+                .get_srt_exchange_rate()
+                .get_srt_amount_as_vst(old_vault.srt_operation_reserved_amount, false).unwrap();
             let tolerance = old_vault.vst_operation_reserved_amount
                 + old_vault.vst_operation_receivable_amount
+                + old_srt_operation_reserved_amount_as_vst
                 - div_util(
                     vrt_amount as u128 * old_vault.get_net_asset_value_as_vst().unwrap() as u128,
                     old_vault.vrt_supply,
