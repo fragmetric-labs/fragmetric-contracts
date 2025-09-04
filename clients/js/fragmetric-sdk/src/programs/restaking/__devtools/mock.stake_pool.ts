efebf7004204916967150902ffb2fe326fce12b3
Terry Kang <maple19out@naver.com>
2025-09-04 15:29:07 +0900

    feat(sdk): elaborate StakePool struct encoder and decoder

--- clients/js/fragmetric-sdk/src/programs/restaking/__devtools/mock.stake_pool.ts
+++ clients/js/fragmetric-sdk/src/programs/restaking/__devtools/mock.stake_pool.ts
@@ -3,15 +3,24 @@ import {
   Decoder,
   fixDecoderSize,
   getAddressDecoder,
-  getAddressEncoder,
   getArrayDecoder,
-  getBytesDecoder,
-  getProgramDerivedAddress,
   getStructDecoder,
+  getBytesDecoder,
+  getU8Decoder,
   getU32Decoder,
   getU64Decoder,
-  getU8Decoder,
+  getOptionDecoder,
+  Option,
+  Encoder,
+  fixEncoderSize,
+  getAddressEncoder,
+  getStructEncoder,
+  getBytesEncoder,
+  getU8Encoder,
+  getU64Encoder,
+  getOptionEncoder,
   ReadonlyUint8Array,
+  getProgramDerivedAddress,
 } from '@solana/kit';
 
 // ref: https://github.com/solana-foundation/anchor/blob/master/ts/packages/spl-stake-pool/idl.json
@@ -30,7 +39,12 @@ export type StakePool = {
   totalLamports: bigint;
   poolTokenSupply: bigint;
   lastUpdateEpoch: bigint;
-  raw: ReadonlyUint8Array;
+  lockUp: ReadonlyUint8Array,
+  epochFee: ReadonlyUint8Array,
+  nextEpochFee: ReadonlyUint8Array,
+  preferredDepositValidatorVoteAddress: Option<Address>,
+  preferredWithdrawValidatorVoteAddress: Option<Address>,
+  raw: ReadonlyUint8Array; 
 };
 
 export function getStakePoolDecoder(): Decoder<StakePool> {
@@ -48,7 +62,36 @@ export function getStakePoolDecoder(): Decoder<StakePool> {
     ['totalLamports', getU64Decoder()],
     ['poolTokenSupply', getU64Decoder()],
     ['lastUpdateEpoch', getU64Decoder()],
-    ['raw', fixDecoderSize(getBytesDecoder(), 329)],
+    ['lockUp', fixDecoderSize(getBytesDecoder(), 48)],
+    ['epochFee', fixDecoderSize(getBytesDecoder(), 16)],
+    ['nextEpochFee', fixDecoderSize(getBytesDecoder(), 24)],
+    ['preferredDepositValidatorVoteAddress', getOptionDecoder(getAddressDecoder())],
+    ['preferredWithdrawValidatorVoteAddress', getOptionDecoder(getAddressDecoder())],
+    ['raw', fixDecoderSize(getBytesDecoder(), 175)],
+  ]);
+}
+
+export function getStakePoolEncoder(): Encoder<StakePool> {
+  return getStructEncoder([
+    ['accountType', fixEncoderSize(getBytesEncoder(), 1)],
+    ['manager', getAddressEncoder()],
+    ['staker', getAddressEncoder()],
+    ['stakeDepositAuthority', getAddressEncoder()],
+    ['stakeWithdrawBumpSeed', getU8Encoder()],
+    ['validatorList', getAddressEncoder()],
+    ['reserveStake', getAddressEncoder()],
+    ['poolMint', getAddressEncoder()],
+    ['managerFeeAccount', getAddressEncoder()],
+    ['tokenProgramId', getAddressEncoder()],
+    ['totalLamports', getU64Encoder()],
+    ['poolTokenSupply', getU64Encoder()],
+    ['lastUpdateEpoch', getU64Encoder()],
+    ['lockUp', fixEncoderSize(getBytesEncoder(), 48)],
+    ['epochFee', fixEncoderSize(getBytesEncoder(), 16)],
+    ['nextEpochFee', fixEncoderSize(getBytesEncoder(), 24)],
+    ['preferredDepositValidatorVoteAddress', getOptionEncoder(getAddressEncoder())],
+    ['preferredWithdrawValidatorVoteAddress', getOptionEncoder(getAddressEncoder())],
+    ['raw', fixEncoderSize(getBytesEncoder(), 175)],
   ]);
 }
 
